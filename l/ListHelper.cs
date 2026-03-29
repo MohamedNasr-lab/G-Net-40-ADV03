6deb5c875bfda40a206f2830283757c55a9af900
MohamedNasr <mohamednasr27122@gmail.com>
2026-03-29 04:51:50 +0200

    Exercise 1

---
+++ l/ListHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l
+{
+    public class ListHelper
+    {
+        public static void printlist<T>(string s,List<T>numbers)
+        {
+            Console.WriteLine($"{s } : {string.Join(", ", numbers)}");
+        }
+        public static void printlistCount<T>(List<T> numbers)
+        {
+            Console.WriteLine($"List Count:{numbers.Count()}");
+        }
+        public static void PrintFirstGrade<T>(List<T> numbers)
+        {
+            Console.WriteLine($"First Grade is:{numbers[0]}");
+        }
+        public static void PrintLastGrade<T>(List<T> numbers)
+        {
+            Console.WriteLine($"Last Grade: {numbers[numbers.Count - 1]}");
+        }
+        public static void GetFirstGradeAbove(int num, List<int> numbers)
+        {
+            int first = numbers.Find(n => n > num);
+            Console.WriteLine($"First Grade Above {num} is {first}");
+        }
+        public static void printlist<T>( List<T> numbers)
+        {
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
--- l/Program.cs
+++ l/Program.cs
@@ -10,7 +10,24 @@ namespace l
     {
         static void Main(string[] args)
         {
-
+            #region Ex1
+            List<int> numbers = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+            ListHelper.printlist("original",numbers);
+            ListHelper.printlistCount(numbers);
+            ListHelper.PrintFirstGrade(numbers);
+            ListHelper.PrintLastGrade(numbers);
+            ListHelper.GetFirstGradeAbove(90,numbers);
+            numbers.Sort();
+            ListHelper.printlist("After Sorting ", numbers);
+            var result = numbers.Where(n => n < 75).ToList();
+            ListHelper.printlist("Grades Below 75", result);
+            numbers.RemoveAll(n => n < 75);
+            ListHelper.printlist("After Removing Below 75", numbers);
+            bool has100 = numbers.Contains(100);
+            Console.WriteLine(has100);
+            List<string> strings = numbers.ConvertAll(n => $"Grade: {n}");
+            ListHelper.printlist( strings);
+            #endregion
         }
     }
 }
